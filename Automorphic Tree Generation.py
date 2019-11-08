daeb7d4b5912ce4b648faafdfb5f9024b47d35f3
MaciejSkrabski <maciej.skrabski@gmail.com>
2019-11-08 14:50:47 +0100

    Added swaping and formating that kinda helps

--- Automorphic Tree Generation.py
+++ Automorphic Tree Generation.py
@@ -1,32 +1,27 @@
-#n - levels, p - vertices
+from swap import swap
+
+
+# n - levels, p - vertices
 def nodesOnLvl(n, p):
     return p**n
 
 
 def totalNodes(n, p):
-    sum=0
+    sum = 0
     for i in range(0, n):
-        sum+=nodesOnLvl(i, p)
-        #print("(p={})^(i={})={}".format(p, i, p**i))
-        #print(sum)
+        sum += nodesOnLvl(i, p)
     return (sum)
 
 
-
-#print(nodesOnLvl(3,4))
-
-
 def generateTree(n, p):
-    arr=[]
-    tree=[]
-    if (n<1 or p<1):
+    arr = []
+    tree = []
+    if (n < 1 or p < 1):
         print("złe dane")
         return None
-    if (n==1):
+    if (n == 1):
         return [1]
-    #print("Total Nodes: {}".format(totalNodes(n,p)))
-    #initialization of all the nodes in the tree
-    for i in range(totalNodes(n,p), 0, -1):
+    for i in range(totalNodes(n, p), 0, -1):
         arr.append(i)
 
     '''print("p%nodesOnLvl({}, {}) : {}".format(n, p, p%nodesOnLvl(n, p)))
@@ -34,79 +29,78 @@ def generateTree(n, p):
     print("Array of all notches!: ")
     print(arr)
     print("NodesOnLvl {}: {}".format(4, nodesOnLvl(3, p)))'''
-    
-    #listki i ich rodzice
-    #THIS IS NICE
-    temp=[]
-    for i in range(0,nodesOnLvl(n-1,p)):
+
+    # listki i ich rodzice
+    temp = []
+    for i in range(0, nodesOnLvl(n-1, p)):
         temp.append(arr.pop())
-        if ((i+1)%p==0 and i!=0):
-            #tree.append(temp)
-            #tree.append(arr.pop(0))
+        if ((i+1) % p == 0 and i != 0):
             tree.append([arr.pop(), temp])
-            temp=[]
-    
+            temp = []
 
     '''print("Tree length: "+ str(len(tree)))
     print("tree for now: ", tree)
     print("arr length at this moment: ", len(arr), "\n", arr)'''
-    
 
-    #generowanie wyższych gałęzi - not bad, ale refactor się przyda. Pomyślę czy da się listki i gałęzie załatwić za jednym zamachem. Wygląda nieskomplikowanie, ale teraz nie mam do tego głowy.
-    l=n-2
-    for j in range (l, 0, -1):
-        #print("j= ", j)
-        
-        #print("temp:",temp)
-        temp2=[]
-        for i in range(0,nodesOnLvl(j,p)):
-            #print("nodes on lvl: ",nodesOnLvl(j,p))
+    # generowanie wyższych gałęzi - not bad, ale refactor się przyda. Pomyślę czy da się listki i gałęzie załatwić za jednym zamachem. Wygląda nieskomplikowanie, ale teraz nie mam do tego głowy.
+    l = n-2
+    for j in range(l, 0, -1):
+        temp2 = []
+        for i in range(0, nodesOnLvl(j, p)):
             temp2.append(tree[i])
-            #print("i= ",i,"temp length: ",len(temp))
-            if ((i+1)%p==0 and i!=0):
+            if ((i+1) % p == 0 and i != 0):
                 temp.append([arr.pop(), temp2])
-
-                temp2=[]
-            #tree.append(arr.pop(0))
-                #print(i)
-
-        #print("TEMP",temp)
-        if(nodesOnLvl(j,p) !=0):
-            tree=temp
-        #print("TREE",tree)
-        temp=[]
-    #print("temp:",temp)
-    
-
-
-    #loop goes 16 times in 4x4 example
-    #for i in range(0, len(tree), 2):
-     #       temp.append(tree[i])
-      #      temp.append(tree[i+1])
-    
-    
-    
+                temp2 = []
+        if(nodesOnLvl(j, p) != 0):
+            tree = temp
+        temp = []
 
     return tree
 
 
+def st(t):
+    return ('{}'.format(t).replace(
+        "[", '\t').replace("]", ''))
 
-#3 lvls, 4 vertices for each node
-tree =  generateTree(4,2)
-print('\n\n', tree, len(tree))
 
-tree =  generateTree(2,4)
-print('\n\n', tree, len(tree))
+# lvls, vertices
+tree = generateTree(4, 2)
+print('\n\n{}'.format(tree))
+print(st(tree))
 
-tree2= generateTree(3,15)
-print('\n\n', tree2, len(tree))
+tree[0][1][0], tree[0][1][1] = swap(tree[0][1][0], tree[0][1][1])
+print(st(tree))
 
-tree3=generateTree(10,5)
-print('\n\n', tree3, len(tree))
+
+tree2 = generateTree(2, 4)
+print('\n\n{}'.format(tree2))
+print(st(tree2))
+tree2[0][1][0], tree2[0][1][1] = swap(tree2[0][1][0], tree2[0][1][1])
+print(st(tree2))
+
+tree3 = generateTree(3, 15)
+print('\n\n{}'.format(tree3))
+print(st(tree3))
+
+print("\n\n")
+print(tree3[0][1])
+tree3[0][1][3][1], tree3[0][1][4][1] = swap(
+    tree3[0][1][3][1], tree3[0][1][4][1])
+print("\n")
+print(tree3[0][1])
+
+'''
+# generuje sie szybko, drukowanie zabiera duzo czasu
+tree4 = generateTree(10, 5)
+# print('\n\n{}'.format(tree3))
+# print(st(tree4))
+
+
+print('\n\n{}'.format(tree4).replace(", [", "\n").replace(
+    "[", "\t").replace(']', ''), "\n\tlenght: ", len(tree4))
+'''
+
 # ^ raczej próba wydajności niż faktyczne sprawdzenie
 
 
-
-#x, y = [], []
-
-
+# x, y = [], []
