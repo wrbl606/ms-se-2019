1243a6ea5ced80f898ab0ae9e17f4babf557d01c
Marcin Wróblewski <marcinwroblewski@outlook.com>
2020-01-25 19:41:08 +0100

    color palette change

--- drzewa/src/App.js
+++ drzewa/src/App.js
@@ -1,29 +1,39 @@
 import React from 'react'
 import {
-  makeStyles
+  makeStyles, createMuiTheme
 } from '@material-ui/core/styles'
 
 import CssBaseline from '@material-ui/core/CssBaseline'
+import ThemeProvider from '@material-ui/styles/ThemeProvider'
 import NavBar from './components/NavBar'
 import TreeView from './components/TreeView'
 import SideBar from './components/SideBar'
 
-const SidebarStyle = makeStyles(theme => ({
+const useStyles = makeStyles({
   root: {
     display: 'flex'
   }
-}))
+});
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {main: '#455A64'},
+    secondary: {main: '#FFC107'}
+  }
+})
 
 function AppContainer () {
-  const classes = SidebarStyle()
+  const classes = useStyles()
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <NavBar />
-      <TreeView />
-      <SideBar />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <CssBaseline />
+        <NavBar />
+        <TreeView />
+        <SideBar />
+      </div>
+    </ThemeProvider>
   )
 }
 
